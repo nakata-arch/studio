f130f86007bd7e1bad58dc4eda57869c25c8d109
中田雄斗 <info.yuuto@gmail.com>
2026-03-29 02:58:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8319571679-fb38f",
+  "appId": "1:34460193112:web:749fb8f5fe8332f4fdd52d",
+  "apiKey": "AIzaSyDcncf-hd0Sgt422cVDtV6RBfLCJIIZGSI",
+  "authDomain": "studio-8319571679-fb38f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "34460193112"
+};
